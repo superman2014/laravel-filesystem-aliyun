166931b1f24730f0af408bf5776209e1b9eefd21
mark <jingyu525@gmail.com>
2017-03-25 21:05:03 +0800

    feat: init commit

---
+++ src/AliyunServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Superman2014\Filesystem\Aliyun;
+
+use Superman2014\Filesystem\Aliyun\Plugins\PutFile;
+use Superman2014\Filesystem\Aliyun\Plugins\PutRemoteFile;
+use Illuminate\Storage\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use OSS\OssClient;
+
+class AliyunServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Storage::extend(
+            'aliyun',
+            function ($app, $config) {
+                $client = new OssClient(
+                    $config['access_id'],
+                    $config['access_key'],
+                    $config['endpoint'],
+                    $config['isCName']
+                );
+                $adapter = new AliOssAdapter(
+                    $client,
+                    $config['bucket'],
+                    $config['debug']
+                );
+
+                $filesystem = new Filesystem($adapter);
+
+                $filesystem->addPlugin(new PutFile());
+                $filesystem->addPlugin(new PutRemoteFile());
+
+                return $filesystem;
+            }
+        );
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+}
